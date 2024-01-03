eeb7edee81785dc34f6b1c9defc8501b076d6412
Damien MIREMONT <damien.miremont@gmail.com>
2024-01-03 16:12:46 +0100

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      colors: {
+        bg_app: "#FDF7F5",
+        bg_ui: "#F6EDEA",
+        bg_hover: "#EFE4DF",
+        border: "#DFCDC5",
+        border_hover: "#C2A499",
+        text: "#43302B",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
